07ec6112d860eb343fb9195c4f76125566f1ecfe
Darío Blanco <dario.blanco@edelight.de>
2013-02-02 18:57:26 +0100

    Use sorted set for the task filters

--- sayit/database.py
+++ sayit/database.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import time
-from datetime import datetime
+from datetime import timedelta
 
 import redis
 from pbkdf2 import crypt
@@ -45,120 +45,61 @@ class User(object):
 
 
 class Task(object):
-    def __init__(self, username, title, completed=False):
+    def __init__(self, username, title, status='uncompleted'):
         self.username = username
-        self.timestamp = int(time.time()) + 1  # The taskid
-        dt = datetime.now()
-        ic = dt.isocalendar()  # year/weeknumber/weekday
+        self.timestamp = time.time()  # Float creation time
+        self.task_id = repr(self.timestamp)  # The taskid
         self.data = {
-            'day': dt.strftime("%d/%m/%Y"),
-            'week': "{0}/{1}".format(ic[1], ic[0]),
+            'username': username,
             'title': title,
-            'completed': completed
+            'status': status
         }
 
     def save(self):
-        # Hash of task with user
-        rd.hmset('task:{0}:user:{1}'.format(self.timestamp,
-                                            self.username),
-                 self.data)
-        # List of tasks ids per day and user
-        rd.sadd('tasksday:{0}:user:{1}'.format(self.data['day'],
-                                               self.username),
-                 self.timestamp)
-        # List of tasks ids per week and user
-        rd.sadd('tasksweek:{0}:user:{1}'.format(self.data['week'],
-                                                self.username),
-                 self.timestamp)
-        # List of completed or uncompleted tasks
-        rd.sadd('tasks.completed:{0}:user:{1}'.format(self.data['completed'],
-                                                      self.username),
-                 self.timestamp)
+        # Hash of task with the full timestamp as id
+        rd.hmset('task:{0}'.format(self.task_id), self.data)
+        # Set the filters as a sorted set of task keys
+        score = timedelta(seconds=self.timestamp).days
+        # Ordered tasks belonging to a user
+        rd.zadd('tasks.all:{0}'.format(self.username), score, self.task_id)
+        # Ordered completed or uncompleted tasks belonging to a user
+        rd.zadd('tasks.{0}:{1}'.format(self.data['status'], self.username),
+                score, self.task_id)
 
     @classmethod
-    def remove(cls, username, task_id):
-        key = 'task:{0}:user:{1}'.format(task_id, username)
+    def remove(cls, task_id):
+        key = 'task:{0}'.format(task_id)
         data = rd.hgetall(key)
-        rd.srem('tasksday:{0}:user:{1}'.format(data['day'], username),
-                task_id)
-        rd.srem('tasksweek:{0}:user:{1}'.format(data['week'], username),
-                task_id)
-        rd.srem('tasks.completed:{0}:user:{1}'.format(data['completed'],
-                                                      username),
+        rd.zrem('tasks.all:{0}'.format(data['username']), task_id)
+        rd.zrem('tasks.{0}:{1}'.format(data['status'], data['username']),
                 task_id)
         rd.delete(key)
 
     @classmethod
-    def edit_title(cls, username, task_id, title):
-        rd.hset('task:{0}:user:{1}'.format(task_id, username), 'title', title)
+    def edit_field(cls, task_id, field_name, field_value):
+        task_key = 'task:{0}'.format(task_id)
+        if field_name == 'status':
+            # Extra logic for the status field change
+            data = rd.hgetall(task_key)
+            if not (data['status'] == field_value):
+                # Remove from the previous state list
+                tasks_key = 'tasks.{0}:{1}'.format(data['status'],
+                                                   data['username'])
+                score = rd.zscore(tasks_key)
+                rd.zrem(tasks_key, task_id)
+                # Add to the new state list
+                rd.add('tasks.{0}:{1}'.format(field_value, data['username']),
+                       score, task_id)
+        rd.hset(task_key, field_name, field_value)
 
     @classmethod
-    def edit_status(cls, username, task_id, completed):
-        key = 'task:{0}:user:{1}'.format(task_id, username)
-        prev_completed = rd.hget(key, 'completed') in ['True', 'true', 'TRUE']
-        # If the new state is different
-        if not (prev_completed == completed):
-            # Remove from the previous state list
-            rd.srem('tasks.completed:{0}:user:{1}'.format(prev_completed,
-                                                          username),
-                    1, task_id)
-            # Add to the new state list
-            rd.sadd('tasks.completed:{0}:user:{1}'.format(completed,
-                                                           username),
-                     task_id)
-            rd.hset('task:{0}:user:{1}'.format(task_id, username),
-                    'completed', completed)
-
-    @classmethod
-    def get_tasks_by_week(cls, username):
-        """Returns a list of weeks with tasks for the specified user"""
-        week_tasks = {}
-
-        key_weeks = rd.keys('tasksweek:*:user:{0}'.format(username))
-        for key_week in key_weeks:
-            task_ids = rd.smembers(key_week)  # Get task ids by week
-            # Get task data
-            week_task_list = []
-            for task_id in task_ids:
-                task_data = rd.hgetall("task:{0}:user:{1}".format(task_id,
-                                                                  username))
-                task_data['timestamp'] = task_id
-                week_task_list.append(task_data)
-            # For humanizing, the week is the first day which have tasks
-            first_week_day = week_task_list[-1:][0]['day']
-            week_tasks[first_week_day] = week_task_list
-
-        return week_tasks
-
-    @classmethod
-    def get_tasks_by_day(cls, username):
-        """Returns a list of days with tasks for the specified user"""
-        day_tasks = {}
-
-        key_days = rd.keys('tasksday:*:user:{0}'.format(username))
-        for key_day in key_days:
-            day = key_day.rstrip('user:{0}'.format(username))
-            day = day.lstrip('tasksday:')
-            task_ids = rd.smembers(key_day)  # Get task ids by day
-            day_tasks[day] = []
-            for task_id in task_ids:
-                task_data = rd.hgetall("task:{0}:user:{1}".format(task_id,
-                                                                  username))
-                task_data['timestamp'] = task_id
-                day_tasks[day].append(task_data)
-
-        return day_tasks
-
-    @classmethod
-    def get_tasks_by_status(cls, username, status):
-        """Returns a list of completed tasks for the specified user"""
+    def get_tasks(cls, username, status='all', limit=20):
+        """Returns a list of tasks for the specified user"""
         tasks = []
-
-        task_ids = rd.smembers(
-            "tasks.completed:{0}:user:{1}".format(status, username))
+        task_ids = rd.zrevrange("tasks.{0}:{1}".format(status, username),
+                                0, limit)
         for task_id in task_ids:
-            task_data = rd.hgetall("task:{0}:user:{1}".format(task_id,
-                                                              username))
+            task_data = rd.hgetall("task:{0}".format(task_id))
             task_data['timestamp'] = task_id
             tasks.append(task_data)
 
